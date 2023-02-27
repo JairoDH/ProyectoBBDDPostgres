7da3d04181f7d9e2c19b3e9bb91654d854689bf9
JairoDH <jairodominguez3594@gmail.com>
2023-02-27 21:20:55 +0100

    Correcciones
    Correcciones de las funciones.

--- Funciones.py
+++ Funciones.py
@@ -32,7 +32,7 @@ def mostrar_trabajadores(db):
         cursor.execute(sql)
         registros = cursor.fetchall()
         for registro in registros:
-            print(registro[0],registros[1],registros[2],registro[3])
+            print(registro[0],registro[1],registro[2],registro[3])
     except:
         print("Error en la consulta")
     db.close()
@@ -68,7 +68,7 @@ def informacion_matricula(db):
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql, matricula_camion)
+        cursor.execute(sql, (matricula_camion,))
         registro = cursor.fetchone()
         while registro:
             print(registro[0],registro[1],registro[2],registro[3],registro[4],registro[5],registro[6],registro[7],registro[8],registro[9])
@@ -85,11 +85,11 @@ def nuevo_camion(db):
     fecha = input("Introduce la fecha de alta (YYYY-MM-DD): ")
     peso = int(input("Introduce el peso máximo a transportar: "))
 
-    sql = "INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES ('{matri}', '{fecha}', {peso})"
+    sql = "INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES (%s, %s, %s)"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql,matri,fecha,peso)
+        cursor.execute(sql, (matri, fecha, peso))
         db.commit()
     except:
         db.rollback()
@@ -110,8 +110,8 @@ def eliminar_camion(db):
         resultado = cursor.fetchone()
         if resultado is not None:
             matricula_camion = resultado[0]
-            borrar = "DELETE FROM CAMION WHERE matricula = '{matricula_camion}"
-            cursor.execute(borrar, matricula_camion)
+            borrar = 'DELETE FROM CAMION WHERE matricula = %s'
+            cursor.execute(borrar, (matricula_camion,))
             db.commit()
             print("Camión con matrícula {matricula_camion} se ha eliminado correctamente.")
     except:
@@ -130,11 +130,11 @@ def actualizar_trabajador(db):
     nuevo_municipio = input("Introduce el nuevo municipio: ")
 
 
-    sql = "UPDATE CONDUCTOR SET telefono = '{nuevo_telefono}', poblacion = '{nuevo_municipio}' WHERE nombre = '{nombre}' AND apellido1 = '{apellido1}'"
+    sql = "UPDATE CONDUCTOR SET telefono = %s, poblacion = %s WHERE nombre = %s AND apellido1 = %s"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql,nombre,apellido1,nuevo_telefono,nuevo_municipio)
+        cursor.execute(sql, (nombre, apellido1, nuevo_telefono, nuevo_municipio))
         if cursor.rowcount == 0:
             print("No se ha encontrado conductor con el nombre {nombre} {apellido1}.")
         else:
