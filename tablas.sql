0388e4d02099569a0f469f8feae63fbf8a0e0262
JairoDH <jairodominguez3594@gmail.com>
2023-03-06 13:51:10 +0100

    modificacion
    modificaciones

--- tablas.sql
+++ tablas.sql
@@ -1,35 +1,32 @@
 CREATE TABLE CAMION(
     matricula varchar(7),
     fecha_alta date,
-    peso_maximo decimal(7,2) NOT NULL
+    peso_maximo float(7,2) NOT NULL,
+CONSTRAINT cp_matricula PRIMARY KEY(matricula),
+CONSTRAINT ch_matri CHECK (matricula REGEXP '[0-9]{4}[A-Z][A-Z][A-Z]'),
+CONSTRAINT ch_fechalta CHECK (TO_CHAR(fecha_alta,'YYYY-MM-DD') <= '2020-01-01')
 );
-ADD CONSTRAINT cp_matricula PRIMARY KEY(matricula),
-ADD CONSTRAINT ch_matri CHECK (REGEXP_LIKE(matricula, '[0-9]{4}[A-Z][A-Z][A-Z]')),
-ADD CONSTRAINT ch_fecha CHECK (fecha_alta BETWEEN to_date ('01/01/2020', 'dd/mm/yyyy') AND to_date ('31/12/2050', 'dd/mm/yyyy'));
-
 CREATE TABLE CAMION_CONDUCTOR(
     matricula_camion varchar(7),
-    codigo_conductor decimal(5),
-    fecha date
+    codigo_conductor int(5),
+    fecha date,
+CONSTRAINT ca_matricami FOREIGN KEY (matricula_camion) references CAMION(matricula),
+CONSTRAINT ca_codicondu FOREIGN KEY (codigo_conductor) references CONDUCTOR(codigo)
 );
-ADD CONSTRAINT ca_matricami FOREIGN KEY (matricula_camion) references CAMION(matricula),
-ADD CONSTRAINT ca_codicondu FOREIGN KEY (codigo_conductor) references CONDUCTOR(codigo);
-
 CREATE TABLE CONDUCTOR(
-    codigo decimal(5),
+    codigo int(5),
     nombre varchar(20),
     apellido1 varchar(20),
     apellido2 varchar(20),
     DNI varchar(9) NOT NULL,
     calle varchar(20),
-    nº_calle decimal(5),
+    nº_calle int(5),
     provincia varchar(10),
     poblacion varchar(10),
-    telefono varchar(9) NOT NULL
+    telefono varchar(9) NOT NULL,
+CONSTRAINT cp_codi PRIMARY KEY (codigo),
+CONSTRAINT un_dni UNIQUE (DNI),
+CONSTRAINT ch_dni CHECK (DNI REGEXP '[0-9]{8}[A-Z]'),
+CONSTRAINT ch_tele check (telefono REGEXP '^[679][0-9]{8}'),
+CONSTRAINT ch_nom check (binary nombre = UPPER (nombre))
 );
-
-ADD CONSTRAINT cp_codi PRIMARY KEY (codigo),
-ADD CONSTRAINT un_dni UNIQUE (DNI),
-ADD CONSTRAINT ch_dni CHECK (REGEXP_LIKE (DNI, '[0-9]{8}[A-Z]')),
-ADD CONSTRAINT ch_tele check (REGEXP_LIKE (telefono, '^[679][0-9]{8}')),
-ADD CONSTRAINT ch_nom check (nombre = UPPER (nombre));
