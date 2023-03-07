1053ae1bea481027a92edb3796501b8112a56897
JairoDH <123737760+JairoDH@users.noreply.github.com>
2023-03-07 08:32:05 +0100

    Corrección
    Corrección de errores

--- tablas.sql
+++ tablas.sql
@@ -3,7 +3,7 @@ CREATE TABLE CAMION(
     fecha_alta date,
     peso_maximo decimal(7,2) NOT NULL,
 CONSTRAINT cp_matricula PRIMARY KEY(matricula),
-CONSTRAINT ch_matri CHECK (matricula REGEXP '[0-9]{4}[A-Z][A-Z][A-Z]'),
+CONSTRAINT ch_matri CHECK (REGEXP_LIKE (matricula,'[0-9]{4}[A-Z][A-Z][A-Z]')),
 CONSTRAINT ch_fecha CHECK (fecha_alta BETWEEN to_date ('01/01/2020', 'dd/mm/yyyy') AND to_date ('31/12/2050', 'dd/mm/yyyy')
 ));
 CREATE TABLE CAMION_CONDUCTOR(
@@ -26,7 +26,7 @@ CREATE TABLE CONDUCTOR(
     telefono varchar(9) NOT NULL,
 CONSTRAINT cp_codi PRIMARY KEY (codigo),
 CONSTRAINT un_dni UNIQUE (DNI),
-CONSTRAINT ch_dni CHECK (DNI REGEXP '[0-9]{8}[A-Z]'),
-CONSTRAINT ch_tele check (telefono REGEXP '^[679][0-9]{8}'),
-CONSTRAINT ch_nom check (binary nombre = UPPER (nombre))
+CONSTRAINT ch_dni CHECK (REGEXP_LIKE(DNI '[0-9]{8}[A-Z]'),
+CONSTRAINT ch_tele check (REGEXP_LIKE(telefono '^[679][0-9]{8}'),
+CONSTRAINT ch_nom check (nombre = UPPER (nombre))
 );
