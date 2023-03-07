c82785a2554af3ef670845655a79c5dc4f44edd5
JairoDH <123737760+JairoDH@users.noreply.github.com>
2023-03-07 08:39:24 +0100

    .
    ..

--- datos.sql
+++ datos.sql
@@ -6,12 +6,12 @@ INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES('3538CGT','2022-0
 INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES('6831FBG','2021-02-16',23000);
 
 
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(11111,'JAIRO','DOMINGUEZ','HIDALGO','49131224G','Tajo',24,'SEVILLA','DOSHERMANS','603835930');
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(22222,'JOSEJOAQUIN','RODRIGUEZ','REINA','47829361R','Marqués',14,'CADIZ','ROTA','658835921');
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(33333,'ELIZABETH','ALFONSO','PRIETO','46204780A','Pañoleta',21,'SEVILLA','MAIRENA','615349290');
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(44444,'LETICIA','HURTADO','PERA','49324356H','London',25,'CORDOBA','CORDOBA','699111221');
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(55555,'CRISTIAN','GOMEZ','TALAMINO','49208495T','Pascual',12,'SEVILLA','DOSHERMANS','719846438');
-INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, nº_calle, provincia, poblacion, telefono) VALUES(66666,'FRANCISCO','BARRERA','GARCIA','49923443F','Martin',16,'HUELVA','LEPE','673892834');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(11111,'JAIRO','DOMINGUEZ','HIDALGO','49131224G','Tajo',24,'SEVILLA','DOSHERMANS','603835930');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(22222,'JOSEJOAQUIN','RODRIGUEZ','REINA','47829361R','Marqués',14,'CADIZ','ROTA','658835921');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(33333,'ELIZABETH','ALFONSO','PRIETO','46204780A','Pañoleta',21,'SEVILLA','MAIRENA','615349290');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(44444,'LETICIA','HURTADO','PERA','49324356H','London',25,'CORDOBA','CORDOBA','699111221');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(55555,'CRISTIAN','GOMEZ','TALAMINO','49208495T','Pascual',12,'SEVILLA','DOSHERMANS','719846438');
+INSERT INTO CONDUCTOR (codigo, nombre, apellido1, apellido2, DNI, calle, n_calle, provincia, poblacion, telefono) VALUES(66666,'FRANCISCO','BARRERA','GARCIA','49923443F','Martin',16,'HUELVA','LEPE','673892834');
 
 
 INSERT INTO CAMION_CONDUCTOR (matricula_camion, codigo_conductor, fecha) VALUES('1849JRH','11111','2022-03-15');
